e3e2e013a2e35adc9b3ae07e0784b34ae0830eae
igoreta <ivoronline@gmail.com>
2020-12-22 16:19:48 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_autowired_property/SpringbootAutowiredPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_autowired_property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutowiredPropertyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAutowiredPropertyApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/ivoronline/springboot_autowired_property/services/MyService.java
@@ -0,0 +1,12 @@
+package com.ivoronline.springboot_autowired_property.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+  public String sayHello() {
+    return "Hello";
+  }
+
+}
